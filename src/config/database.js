9bf40ce374f12788b6cbd32ce8879a72ae53fde2
Matheus Lima <121356147+Matheuziin13@users.noreply.github.com>
2023-07-26 01:23:12 -0300

    task list

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '09279920',
+  database: 'tasklist',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoresAll: true,
+  },
+};
